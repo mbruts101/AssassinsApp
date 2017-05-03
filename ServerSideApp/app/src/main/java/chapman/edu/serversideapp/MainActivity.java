3d80aba688ca93ae73d89ed993048fcb94a53a3c
Michael Brutsch <bruts101@mail.chapman.edu>
2017-05-03 00:22:01 -0400

    Added Server side app

---
+++ ServerSideApp/app/src/main/java/chapman/edu/serversideapp/MainActivity.java
@@ -0,0 +1,30 @@
+package chapman.edu.serversideapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    Server server;
+    TextView infoip, msg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        infoip = (TextView) findViewById(R.id.infoip);
+        msg = (TextView) findViewById(R.id.msg);
+        server = new Server(this);
+        infoip.setText(server.getIpAddress() + ":" + server.getPort());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        server.onDestroy();
+    }
+}
