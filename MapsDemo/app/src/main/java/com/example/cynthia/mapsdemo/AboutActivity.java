f7b884a8428d7e875c603a3923e2c354e3393f3a
mbruts101 <bruts101@mail.chapman.edu>
2017-05-05 12:36:39 -0700

    set onclicklistenters for button

--- MapsDemo/app/src/main/java/com/example/cynthia/mapsdemo/AboutActivity.java
+++ MapsDemo/app/src/main/java/com/example/cynthia/mapsdemo/AboutActivity.java
@@ -4,16 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by User on 5/5/2017.
  */
 
 public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
-
+    public Button backButt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        backButt = (Button) findViewById(R.id.backButton);
+
+        backButt.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
--- MapsDemo/app/src/main/java/com/example/cynthia/mapsdemo/StartActivity.java
+++ MapsDemo/app/src/main/java/com/example/cynthia/mapsdemo/StartActivity.java
@@ -4,15 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by User on 5/5/2017.
  */
 
 public class StartActivity extends AppCompatActivity implements View.OnClickListener{
+    public Button startButt;
+    public Button aboutButt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        startButt = (Button) findViewById(R.id.startButton);
+        aboutButt = (Button) findViewById(R.id.aboutButton);
+
+        startButt.setOnClickListener(this);
+        aboutButt.setOnClickListener(this);
     }
 
     @Override
